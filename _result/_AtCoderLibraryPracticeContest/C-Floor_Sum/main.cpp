201c9ce42672b626db872f728e94b0f411fcecf5
Yoyuuhi <xijie931025@gmail.com>
2023-04-09 16:48:05 +0900

    AtCoderLibraryPracticeContest C-Floor Sum

---
+++ _result/_AtCoderLibraryPracticeContest/C-Floor_Sum/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int t;
+  cin >> t;
+  rep(i, t) {
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    out(floor_sum(n, m, a, b));
+  }
+}
