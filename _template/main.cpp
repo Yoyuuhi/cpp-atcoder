60777c4604b0e40851f392b0f71aeb809af4d203
Yoyuuhi <xijie931025@gmail.com>
2023-04-09 15:57:45 +0900

    fix template

--- _template/main.cpp
+++ _template/main.cpp
@@ -1,12 +1,33 @@
 #include <bits/stdc++.h>
+
+#include <atcoder/all>
 #define out(X) cout << (X) << endl;
 #ifdef __LOCAL
 #define DBG(X) cout << #X << " = " << (X) << endl;
 #else
 #define DBG(X)
 #endif
-
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
 using namespace std;
+using namespace atcoder;
+template <typename T>
+bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 int main() {
 #ifdef __LOCAL
