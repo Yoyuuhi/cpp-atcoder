e6471d9f7af8d07b618a078afbab261c42e9d2ee
Yoyuuhi <xijie931025@gmail.com>
2023-04-10 19:42:37 +0900

    AtCoderBeginnerContest297 A-Double Click

---
+++ _result/_AtCoderBeginnerContest297/A-Double_Click/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, d;
+  cin >> n >> d;
+  int t[n];
+  rep(i, n) { cin >> t[i]; }
+  rep(i, n) {
+    if (i == 0) {
+      continue;
+    }
+    if (t[i] - t[i - 1] <= d) {
+      out(t[i]);
+      return 0;
+    }
+  }
+  out(-1);
+}
