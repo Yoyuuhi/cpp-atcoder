feef1b1e1ba950e4718bb16b95b53cc2471b2136
Yoyuuhi <xijie931025@gmail.com>
2023-04-21 23:42:02 +0900

    AtCoderBeginnerContest231 B-Election

---
+++ _result/_AtCoderBeginnerContest231/B-Election/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+
+  map<string, int> mem;
+  rep(i, n) {
+    string s;
+    cin >> s;
+    mem[s] += 1;
+  }
+
+  string ans = "";
+  int num = 0;
+  for (auto m = mem.begin(); m != mem.end(); m++) {
+    if (m->second > num) {
+      num = m->second;
+      ans = m->first;
+    }
+  }
+  out(ans);
+}
