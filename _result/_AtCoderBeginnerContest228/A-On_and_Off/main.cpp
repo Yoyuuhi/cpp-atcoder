7ae904397caa9bd8e28712ceea76278adaf31dba
Yoyuuhi <xijie931025@gmail.com>
2023-04-28 00:21:36 +0900

    AtCoderBeginnerContest228 A-On and Off

---
+++ _result/_AtCoderBeginnerContest228/A-On_and_Off/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int s, t, x;
+  cin >> s >> t >> x;
+  if (s > t) t += 24;
+  if (x >= s && x < t || x + 24 >= s && x + 24 < t) {
+    out("Yes");
+  } else {
+    out("No");
+  }
+}
