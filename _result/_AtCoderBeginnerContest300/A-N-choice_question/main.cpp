677dc65862d2a1f6ff7a9716e9641f511cc04715
Yoyuuhi <xijie931025@gmail.com>
2023-04-30 00:51:55 +0900

    AtCoderBeginnerContest300 A-N-choice question

---
+++ _result/_AtCoderBeginnerContest300/A-N-choice_question/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, a, b;
+  cin >> n >> a >> b;
+  rep(i, n) {
+    int c;
+    cin >> c;
+    if (a + b == c) {
+      out(i + 1);
+      return 0;
+    }
+  }
+}
