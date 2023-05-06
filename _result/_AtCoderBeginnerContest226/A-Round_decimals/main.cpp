ebc24e302eb5cf631c75b6cc751f4fd0109d00a9
Yoyuuhi <xijie931025@gmail.com>
2023-05-06 15:38:14 +0900

    AtCoderBeginnerContest226 A-Round decimals

---
+++ _result/_AtCoderBeginnerContest226/A-Round_decimals/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  float x;
+  cin >> x;
+  int xx = x * 10;
+  int res = xx % 10;
+  if (res >= 5) {
+    out(xx / 10 + 1);
+  } else {
+    out(xx / 10);
+  }
+}
