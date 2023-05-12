3690c33b17068a1fa909aaefe983a119f3f0dcf7
Yoyuuhi <xijie931025@gmail.com>
2023-05-12 09:39:50 +0900

    AtCoderBeginnerContest223 A-Exact Price

---
+++ _result/_AtCoderBeginnerContest223/A-Exact_Price/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int x;
+  cin >> x;
+  if (x >= 100 && x % 100 == 0) {
+    out("Yes");
+    return 0;
+  }
+  out("No");
+}
