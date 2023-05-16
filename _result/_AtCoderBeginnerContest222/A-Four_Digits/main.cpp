96c81976432a9a3c58879db4f225d41e95aabc13
Yoyuuhi <xijie931025@gmail.com>
2023-05-16 13:31:52 +0900

    AtCoderBeginnerContest222 A-Four Digits

---
+++ _result/_AtCoderBeginnerContest222/A-Four_Digits/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  string n;
+  cin >> n;
+  string ans = "";
+  rep(i, 4 - n.size()) ans += "0";
+  out(ans + n);
+}
