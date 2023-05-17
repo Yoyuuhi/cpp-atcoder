aa7c10bd1f39804c9307dd5ab4fafaa74d9410bd
Yoyuuhi <xijie931025@gmail.com>
2023-05-17 13:24:45 +0900

    AtCoderBeginnerContest222 B-Failing Grade

---
+++ _result/_AtCoderBeginnerContest222/B-Failing_Grade/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, p;
+  cin >> n >> p;
+  int ans = 0;
+  rep(i, n) {
+    int a;
+    cin >> a;
+    if (a < p) ans++;
+  }
+  out(ans);
+}
