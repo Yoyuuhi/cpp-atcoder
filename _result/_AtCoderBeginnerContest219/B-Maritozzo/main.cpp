ac4805b10ba7eb77c5bc5bb55517017fefea3483
Yoyuuhi <xijie931025@gmail.com>
2023-05-25 13:22:52 +0900

    AtCoderBeginnerContest219 B-Maritozzo

---
+++ _result/_AtCoderBeginnerContest219/B-Maritozzo/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  vector<string> ss(3);
+  rep(i, 3) cin >> ss[i];
+  string t;
+  cin >> t;
+
+  string ans = "";
+  rep(i, t.size()) {
+    int idx = t[i] - '1';
+    ans += ss[idx];
+  }
+  out(ans);
+}
