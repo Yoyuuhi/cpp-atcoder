641786050972132bcd70427ad0687d4ad4315384
Yoyuuhi <xijie931025@gmail.com>
2023-06-15 13:18:57 +0900

    AtCoderBeginnerContest212 B-Weak Password

---
+++ _result/_AtCoderBeginnerContest212/B-Weak_Password/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  string x;
+  cin >> x;
+  bool isWeak = true;
+  rep(i, 3) if (x[i] != x[i + 1]) isWeak = false;
+  if (isWeak) {
+    out("Weak");
+    return 0;
+  }
+  isWeak = true;
+  rep(i, 3) if (x[i + 1] - x[i] != 1 && x[i + 1] - x[i] != -9) isWeak = false;
+  if (isWeak) {
+    out("Weak");
+    return 0;
+  }
+  out("Strong");
+}
