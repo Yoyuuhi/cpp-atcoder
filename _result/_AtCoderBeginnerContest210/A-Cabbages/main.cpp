3035cd0d7bf470e7a4e436df9da05116e73b293f
Kishou <xijie931025@gmail.com>
2023-06-18 00:19:23 +0900

    AtCoderBeginnerContest210 A-Cabbages

---
+++ _result/_AtCoderBeginnerContest210/A-Cabbages/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, a, x, y;
+  cin>>n>>a>>x>>y;
+  int ans = 0;
+  if (n<=a) {
+    out(n*x);
+  } else {
+    out(a*x+(n-a)*y);
+  }
+}
