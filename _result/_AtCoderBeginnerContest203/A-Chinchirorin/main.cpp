2cc26da6faf83ff09457e2311b52c82315b2f59b
Yoyuuhi <xijie931025@gmail.com>
2023-07-19 14:53:57 +0900

    AtCoderBeginnerContest203 A-Chinchirorin

---
+++ _result/_AtCoderBeginnerContest203/A-Chinchirorin/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  vector<int> mem(3);
+  rep(i, 3) cin >> mem[i];
+  sort(all(mem));
+  if (mem[0] == mem[1]) {
+    out(mem[2]);
+  } else if (mem[1] == mem[2]) {
+    out(mem[0]);
+  } else {
+    out(0);
+  }
+}
