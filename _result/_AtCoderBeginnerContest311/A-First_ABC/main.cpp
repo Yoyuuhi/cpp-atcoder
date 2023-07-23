cba99c54591fd42347eb402346a46471020c2ddb
Yoyuuhi <xijie931025@gmail.com>
2023-07-23 16:11:14 +0900

    AtCoderBeginnerContest311 A-First ABC

---
+++ _result/_AtCoderBeginnerContest311/A-First_ABC/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  set<char> mem;
+  rep(i, n) {
+    if (mem.find(s[i]) == mem.end()) {
+      mem.insert(s[i]);
+    }
+    if (mem.size() == 3) {
+      out(i + 1);
+      return 0;
+    }
+  }
+}
