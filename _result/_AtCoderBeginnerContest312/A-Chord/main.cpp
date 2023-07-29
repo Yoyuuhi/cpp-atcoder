a8aa03036d98f50a1d2035c16a568beb3fbcf1d9
Yoyuuhi <xijie931025@gmail.com>
2023-07-29 21:03:58 +0900

    AtCoderBeginnerContest312 A-Chord

---
+++ _result/_AtCoderBeginnerContest312/A-Chord/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  set<string> st = {"ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD"};
+  string s;
+  cin >> s;
+  cout << (st.find(s) != st.end() ? "Yes" : "No") << endl;
+}
