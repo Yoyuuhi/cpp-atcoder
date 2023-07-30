4506b7da6ea93a5cfa73fdbe681dc12aa5f5c54f
Yoyuuhi <xijie931025@gmail.com>
2023-07-31 00:11:09 +0900

    AtCoderBeginnerContest201 A-Tiny Arithmetic Sequence

---
+++ _result/_AtCoderBeginnerContest201/A-Tiny_Arithmetic_Sequence/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  vector<int> as(3);
+  rep(i, 3) cin >> as[i];
+  sort(all(as));
+  cout << (as[1] - as[0] == as[2] - as[1] ? "Yes" : "No") << endl;
+}
