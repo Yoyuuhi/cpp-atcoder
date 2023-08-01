1e100700254c01933c5d254bd57cb680c15d8839
Yoyuuhi <xijie931025@gmail.com>
2023-08-01 13:27:58 +0900

    AtCoderBeginnerContest201 B-Do you know the second highest mountain?

---
+++ _result/_AtCoderBeginnerContest201/B-Do_you_know_the_second_highest_mountain?/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  vector<pair<int, string>> tss(n);
+  rep(i, n) {
+    string s;
+    int t;
+    cin >> s >> t;
+    tss[i] = {t, s};
+  }
+  sort(all(tss));
+  out(tss[n - 2].second);
+}
