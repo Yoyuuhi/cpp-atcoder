0a2c2de1535e0216e4634bc629a80c653dcd3963
Yoyuuhi <xijie931025@gmail.com>
2023-08-06 16:42:42 +0900

    AtCoderBeginnerContest313 A-To Be Saikyo

---
+++ _result/_AtCoderBeginnerContest313/A-To_Be_Saikyo/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  vector<int> ps(n);
+  rep(i, n) cin >> ps[i];
+  int m = ps[0];
+  rep(i, n) m = max(m, ps[i]);
+  rep2(i, 1, n) if (m == ps[i]) m++;
+  out(m - ps[0]);
+}
