8738add35394b3c3eb659cf2d0aae2dee47a54a5
Yoyuuhi <xijie931025@gmail.com>
2023-08-13 13:42:46 +0900

    AtCoderBeginnerContest314 A-3.14

---
+++ _result/_AtCoderBeginnerContest314/A-3.14/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  string s =
+      "3."
+      "141592653589793238462643383279502884197169399375105820974944592307816406"
+      "2862089986280348253421170679";
+  rep(i, n + 2) { cout << s[i]; }
+  cout << endl;
+}
