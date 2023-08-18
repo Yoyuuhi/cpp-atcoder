6230c9cf0302079c360714efd859c0a080ad0000
Yoyuuhi <xijie931025@gmail.com>
2023-08-18 13:22:07 +0900

    AtCoderBeginnerContest196 B-Round Down

---
+++ _result/_AtCoderBeginnerContest196/B-Round_Down/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  string x;
+  cin >> x;
+  rep(i, x.size()) {
+    if (x[i] == '.') break;
+    cout << x[i];
+  }
+  cout << endl;
+}
