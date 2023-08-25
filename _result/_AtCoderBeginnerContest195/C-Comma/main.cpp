16cc51d2dd1bb4d26ac0c9b9d865a9e17a424965
Yoyuuhi <xijie931025@gmail.com>
2023-08-25 20:41:52 +0900

    AtCoderBeginnerContest195 C-Comma

---
+++ _result/_AtCoderBeginnerContest195/C-Comma/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n;
+  cin >> n;
+  ll o = 1000;
+
+  ll ans = 0;
+  while (n >= o) {
+    ans += n - o + 1;
+    o *= 1000;
+  }
+  out(ans);
+}
