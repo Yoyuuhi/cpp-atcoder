0e5ca4deeed253c41018fbe99f7a4531b2bd39ce
Yoyuuhi <xijie931025@gmail.com>
2023-09-09 21:09:21 +0900

    AtCoderBeginnerContest319 B-Measure

---
+++ _result/_AtCoderBeginnerContest319/B-Measure/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  rep(i, n + 1) {
+    int ans = INF;
+    rep2(j, 1, 10) {
+      if (n % j) continue;
+      int nj = n / j;
+      if (!(i % nj)) ans = min(ans, j);
+    }
+    if (ans == INF) {
+      cout << "-";
+    } else {
+      cout << ans;
+    }
+  }
+  cout << endl;
+}
