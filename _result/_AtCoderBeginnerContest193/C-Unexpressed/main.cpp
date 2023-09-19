db79d23b774f3a51299a80cd4a4cdaa9fda8fe95
Yoyuuhi <xijie931025@gmail.com>
2023-09-19 14:20:21 +0900

    AtCoderBeginnerContest193 C-Unexpressed

---
+++ _result/_AtCoderBeginnerContest193/C-Unexpressed/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n;
+  cin >> n;
+
+  set<ll> s;
+  for (ll i = 2; i * i <= n; i++) {
+    for (ll j = 2; pow(i, j) <= n; j++) {
+      s.insert(pow(i, j));
+    }
+  }
+  out(n - s.size());
+}
