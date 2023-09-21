14cba25ad6eeb6489231dc8eadb1ba2a2b9c359b
Yoyuuhi <xijie931025@gmail.com>
2023-09-21 13:37:27 +0900

    AtCoderBeginnerContest187 C-1-SAT

---
+++ _result/_AtCoderBeginnerContest187/C-1-SAT/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  map<string, bool> mem;
+  rep(i, n) {
+    string s;
+    cin >> s;
+    mem[s] = true;
+  }
+  for (auto m = mem.begin(); m != mem.end(); m++) {
+    string s = m->first;
+    string ss = '!' + s;
+    if (mem.find(ss) != mem.end()) {
+      out(s);
+      return 0;
+    }
+  }
+  out("satisfiable");
+}
