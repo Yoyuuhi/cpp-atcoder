7a6ef6e9712cf5b909dc6ab4c1603c7e067d56c9
Yoyuuhi <xijie931025@gmail.com>
2023-09-23 21:02:28 +0900

    AtCoderBeginnerContest321 A-321-like Checker

---
+++ _result/_AtCoderBeginnerContest321/A-321-like_Checker/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  int num = -1;
+  while (n) {
+    if (n % 10 <= num) {
+      out("No");
+      return 0;
+    }
+    num = n % 10;
+    n /= 10;
+  }
+  out("Yes");
+}
