37b87d64333815c87089420620e39b0c19656fc2
Yoyuuhi <xijie931025@gmail.com>
2023-09-27 00:45:08 +0900

    AtCoderBeginnerContest176 C-Step

---
+++ _result/_AtCoderBeginnerContest176/C-Step/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  ll bef = 0;
+  ll ans = 0;
+  rep(i, n) {
+    ll a;
+    cin >> a;
+    ans += max(ll(0), bef - a);
+    bef = max(bef, a);
+  }
+  out(ans);
+}
