cd030bf1293ac8cd1a549aba4eacd8ef367c0380
Yoyuuhi <xijie931025@gmail.com>
2023-09-30 21:05:50 +0900

    AtCoderBeginnerContest322 B-Prefix and Suffix

---
+++ _result/_AtCoderBeginnerContest322/B-Prefix_and_Suffix/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, m;
+  cin >> n >> m;
+  string s, t;
+  cin >> s >> t;
+
+  int i = m - n;
+  if (s == t.substr(0, n) && s == t.substr(i, n)) {
+    out(0);
+    return 0;
+  }
+  if (s == t.substr(0, n)) {
+    out(1);
+    return 0;
+  }
+  if (s == t.substr(i, n)) {
+    out(2);
+    return 0;
+  }
+  out(3);
+}
