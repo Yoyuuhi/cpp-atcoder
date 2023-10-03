5c5eecf38fc66062fe1a76c6e98637239a169c10
Yoyuuhi <xijie931025@gmail.com>
2023-10-03 23:32:26 +0900

    AtCoderBeginnerContest161 C-

---
+++ _result/_AtCoderBeginnerContest161/C-/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n, k;
+  cin >> n >> k;
+  cout << min(n % k, -(n % k - k)) << endl;
+}
