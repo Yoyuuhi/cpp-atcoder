1577dcbde3943b14ec0380b686467ea116c7cd75
Yoyuuhi <xijie931025@gmail.com>
2023-10-08 18:07:31 +0900

    AtCoderBeginnerContest323 A-Weak Beats

---
+++ _result/_AtCoderBeginnerContest323/A-Weak_Beats/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  string s;
+  cin >> s;
+  rep2(i, 1, s.size()) {
+    if (!(i % 2)) continue;
+    if (s[i] == '1') {
+      out("No");
+      return 0;
+    }
+  }
+  out("Yes");
+}
