8841846b84403f8a611133c7e1522fe16149d0c4
Yoyuuhi <xijie931025@gmail.com>
2023-10-14 21:02:16 +0900

    AtCoderBeginnerContest324 A-Same

---
+++ _result/_AtCoderBeginnerContest324/A-Same/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  vector<int> as(n);
+  rep(i, n) cin >> as[i];
+  bool poss = true;
+  int a = as[0];
+  rep(i, n) {
+    if (a != as[i]) {
+      out("No");
+      return 0;
+    }
+  }
+  out("Yes");
+}
