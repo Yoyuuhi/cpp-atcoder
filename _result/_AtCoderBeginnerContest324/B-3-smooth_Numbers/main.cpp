3c54fec2756d98590818cdba11b5f593d2aeccc3
Yoyuuhi <xijie931025@gmail.com>
2023-10-14 21:04:27 +0900

    AtCoderBeginnerContest324 B-3-smooth Numbers

---
+++ _result/_AtCoderBeginnerContest324/B-3-smooth_Numbers/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n;
+  cin >> n;
+  while (!(n % 2)) n /= 2;
+  while (!(n % 3)) n /= 3;
+  cout << (n == 1 ? "Yes" : "No") << endl;
+}
