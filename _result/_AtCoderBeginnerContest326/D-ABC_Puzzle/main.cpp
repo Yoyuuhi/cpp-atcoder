f08ad04a0681a10507db803f1c68b5db3555ba31
Yoyuuhi <xijie931025@gmail.com>
2023-10-29 00:05:21 +0900

    AtCoderBeginnerContest326 D-ABC Puzzle

--- _result/_AtCoderBeginnerContest326/D-ABC_Puzzle/main.cpp
+++ _result/_AtCoderBeginnerContest326/D-ABC_Puzzle/main.cpp
@@ -16,120 +16,32 @@ using ll = long long;
 using mint = atcoder::modint998244353;
 const int INF = numeric_limits<int>::max() / 2;
 
-vector<vector<char>> ans;
-vector<int> ds;
 int n;
 string r, c;
-bool solve() {
-  ans.assign(n, vector<char>(n, '.'));
+vector<int> da, db, dc;
+vector<vector<char>> ans;
+bool judge() {
   rep(i, n) {
+    bool find = false;
+    int b = 0;
     rep(j, n) {
-      if (ds[i * n + j] == 1) ans[i][j] = 'X';
+      if (ans[i][j] == '.') continue;
+      if (!find && ans[i][j] != r[i]) return false;
+      if (ans[i][j] == r[i]) find = true;
+      b += 1 << (ans[i][j] - 'A');
     }
-  }
-  rep(i, n) {
-    int count = 0;
-    rep(j, n) {
-      if (ans[i][j] == 'X') count++;
-    }
-    if (count != 3) return false;
+    if (!find || b != 7) return false;
   }
   rep(j, n) {
-    int count = 0;
-    rep(i, n) {
-      if (ans[i][j] == 'X') count++;
-    }
-    if (count != 3) return false;
-  }
-
-  rep(i, n) {
-    char ri = r[i];
-    rep(j, n) {
-      if (ans[i][j] == '.') continue;
-      if (ans[i][j] != 'X' && ans[i][j] != ri) return false;
-      ans[i][j] = ri;
-      break;
-    }
-  }
-  rep(j, n) {
-    char cj = c[j];
+    bool find = false;
+    int b = 0;
     rep(i, n) {
       if (ans[i][j] == '.') continue;
-      if (ans[i][j] != 'X' && ans[i][j] != cj) return false;
-      ans[i][j] = cj;
-      break;
+      if (!find && ans[i][j] != c[j]) return false;
+      if (ans[i][j] == c[j]) find = true;
+      b += 1 << (ans[i][j] - 'A');
     }
-  }
-  rep(i, n) {
-    rep(j, n) {
-      vector<bool> mem(3, false);
-      if (ans[i][j] == '.') continue;
-      if (ans[i][j] == 'X') {
-        rep(x, n) {
-          if (ans[x][j] != '.' && ans[x][j] != 'X') {
-            mem[ans[x][j] - 'A'] = true;
-          }
-        }
-        rep(y, n) {
-          if (ans[i][y] != '.' && ans[i][y] != 'X') {
-            mem[ans[i][y] - 'A'] = true;
-          }
-        }
-        int poss = 0;
-        int idx = 0;
-        rep(a, 3) {
-          if (!mem[a]) {
-            poss++;
-            idx = a;
-          }
-        }
-        if (poss == 0) return false;
-        if (poss == 1) {
-          ans[i][j] = 'A' + idx;
-        }
-      }
-    }
-  }
-  rep(i, n) {
-    rep(j, n) {
-      vector<bool> mem(3, false);
-      if (ans[i][j] == '.') continue;
-      if (ans[i][j] == 'X') {
-        rep(x, n) {
-          if (ans[x][j] != '.' && ans[x][j] != 'X') {
-            mem[ans[x][j] - 'A'] = true;
-          }
-        }
-        rep(y, n) {
-          if (ans[i][y] != '.' && ans[i][y] != 'X') {
-            mem[ans[i][y] - 'A'] = true;
-          }
-        }
-        bool poss = false;
-        rep(a, 3) {
-          if (!mem[a]) {
-            ans[i][j] = 'A' + a;
-            poss = true;
-            break;
-          }
-        }
-        if (!poss) return false;
-      }
-    }
-  }
-  rep(i, n) {
-    vector<int> mem(3, 0);
-    rep(j, n) {
-      if (ans[i][j] != '.') mem[ans[i][j] - 'A']++;
-      if (mem[ans[i][j] - 'A'] > 1) return false;
-    }
-  }
-  rep(j, n) {
-    vector<int> mem(3, 0);
-    rep(i, n) {
-      if (ans[i][j] != '.') mem[ans[i][j] - 'A']++;
-      if (mem[ans[i][j] - 'A'] > 1) return false;
-    }
+    if (!find || b != 7) return false;
   }
   return true;
 }
@@ -140,22 +52,35 @@ int main() {
 #endif
 
   cin >> n;
-
   cin >> r >> c;
 
-  ds.assign(n * n, 0);
-  rep2(i, n * n - 3 * n, n * n) ds[i] = 1;
+  rep(i, n) {
+    da.push_back(i);
+    db.push_back(i);
+    dc.push_back(i);
+  }
   do {
-    if (solve()) {
-      out("Yes");
-      for (auto as : ans) {
-        for (auto a : as) {
-          cout << a;
+    do {
+      do {
+        ans.assign(n, vector<char>(n, '.'));
+        rep(i, n) {
+          ans[i][da[i]] = 'A';
+          ans[i][db[i]] = 'B';
+          ans[i][dc[i]] = 'C';
         }
-        cout << endl;
-      }
-      return 0;
-    }
-  } while (next_permutation(all(ds)));
+        if (judge()) {
+          out("Yes");
+          for (auto as : ans) {
+            for (auto a : as) {
+              cout << a;
+            }
+            cout << endl;
+          }
+          return 0;
+        }
+      } while (next_permutation(all(da)));
+    } while (next_permutation(all(db)));
+  } while (next_permutation(all(dc)));
+
   out("No");
 }
