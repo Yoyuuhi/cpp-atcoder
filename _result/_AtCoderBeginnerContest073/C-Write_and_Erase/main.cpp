a815b35f17a2cd3bc18edb922a77ccb0a87270aa
Yoyuuhi <xijie931025@gmail.com>
2023-11-01 22:38:53 +0900

    AtCoderBeginnerContest073 C-Write and Erase

---
+++ _result/_AtCoderBeginnerContest073/C-Write_and_Erase/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+
+  map<int, int> mem;
+  rep(i, n) {
+    int a;
+    cin >> a;
+    mem[a]++;
+  }
+  int ans = 0;
+  for (auto m : mem) {
+    if (m.second % 2) ans++;
+  }
+  out(ans);
+}
