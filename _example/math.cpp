7aee71e0bfa2300e1aa7fbb7a4ea58968372d6d5
Yoyuuhi <xijie931025@gmail.com>
2023-11-08 00:07:49 +0900

    rename

--- _example/math.cpp
+++ _example/math.cpp
@@ -53,6 +53,26 @@ long long gcd(long long a, long long b) {
   }
 }
 
+/*  lcm2 (a, b) : 2整数版
+    入力：整数 a, b
+    出力：aとbの最小公倍数
+*/
+long long lcm2(long long a, long long b) {
+  long long d = gcd(a, b);
+  return a / d * b;
+}
+/* lcm (vec) : ベクトルでの多整数版
+    入力：整数のベクトル vec
+    出力：すべての要素の最小公倍数
+*/
+long long lcm(const vector<long long> &vec) {
+  long long l = vec[0];
+  for (int i = 0; i < vec.size() - 1; i++) {
+    l = lcm2(l, vec[i + 1]);
+  }
+  return l;
+}
+
 // https://algo-logic.info/divisor/
 vector<long long> divisor(long long n) {
   vector<long long> ret;
