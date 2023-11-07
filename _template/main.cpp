e9744be438ec0ba9e2444971b0d5deecf8d7d65e
Yoyuuhi <xijie931025@gmail.com>
2023-11-08 00:08:10 +0900

    fix template

--- _template/main.cpp
+++ _template/main.cpp
@@ -2,6 +2,7 @@
 
 #include <atcoder/all>
 #define out(X) cout << (X) << endl;
+#define outwoln(X) cout << (X);
 #ifdef __LOCAL
 #define DBG(X) cout << #X << " = " << (X) << endl;
 #else
@@ -10,11 +11,18 @@
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
+#define outv(vs) \
+  for (auto v : vs) out(v)
 using namespace std;
 using namespace atcoder;
 using ll = long long;
 using mint = atcoder::modint998244353;
 const int INF = numeric_limits<int>::max() / 2;
+vector<ll> nis(int n) {
+  vector<ll> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
 
 int main() {
 #ifdef __LOCAL
