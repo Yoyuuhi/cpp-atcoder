705cfd8cca91be4e815d8464e2ea5232f4c175ec
Yoyuuhi <xijie931025@gmail.com>
2023-11-10 22:32:58 +0900

    update template

--- _template/main.cpp
+++ _template/main.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#define out(X) cout << (X) << endl;
-#define outwoln(X) cout << (X);
+#define outl cout << endl;
 #ifdef __LOCAL
 #define DBG(X) cout << #X << " = " << (X) << endl;
 #else
@@ -11,17 +10,35 @@
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
-#define outv(vs) \
-  for (auto v : vs) out(v)
 using namespace std;
 using namespace atcoder;
 using ll = long long;
-using mint = atcoder::modint998244353;
+using mint = atcoder::modint1000000007;
 const int INF = numeric_limits<int>::max() / 2;
-vector<ll> nis(int n) {
-  vector<ll> ret(n);
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
   rep(i, n) cin >> ret[i];
   return ret;
+}
+template <typename T>
+typename enable_if<is_same_v<T, ll> || is_same_v<T, int> ||
+                       is_same_v<T, unsigned int>,
+                   void>::type
+out(T value) {
+  cout << value << endl;
+}
+template <typename T>
+typename enable_if<is_same_v<T, mint>, void>::type out(T value) {
+  cout << value.val() << endl;
+}
+template <typename T>
+typename enable_if<is_same_v<T, vector<ll>> || is_same_v<T, vector<int>>,
+                   void>::type
+out(T value) {
+  for (auto v : value) cout << v << " ";
+  cout << endl;
 }
 
 int main() {
@@ -34,5 +51,5 @@ int main() {
   cin >> a;
   cin >> b;
 
-  out(a + b)
+  out(a + b);
 }
