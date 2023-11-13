a538f18489214442833fcfa8b5701e21b7ba6b03
Yoyuuhi <xijie931025@gmail.com>
2023-11-13 21:03:47 +0900

    AtCoderBeginnerContest058 C-怪文書

---
+++ _result/_AtCoderBeginnerContest058/C-怪文書/main.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+typename enable_if<is_same_v<T, ll> || is_same_v<T, int> ||
+                       is_same_v<T, unsigned int>,
+                   void>::type
+out(T value) {
+  cout << value << endl;
+}
+template <typename T>
+typename enable_if<is_same_v<T, mint>, void>::type out(T value) {
+  cout << value.val() << endl;
+}
+template <typename T>
+typename enable_if<is_same_v<T, vector<ll>> || is_same_v<T, vector<int>>,
+                   void>::type
+out(T value) {
+  for (auto v : value) cout << v << " ";
+  cout << endl;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  vector<vector<int>> mem(n, vector<int>(26, 0));
+  rep(i, n) {
+    string s;
+    cin >> s;
+    for (auto c : s) {
+      mem[i][c - 'a']++;
+    }
+  }
+
+  string ans = "";
+  rep(i, 26) {
+    int m = INF;
+    for (auto me : mem) {
+      m = min(m, me[i]);
+    }
+    rep(j, m) { ans += 'a' + i; }
+  }
+  cout << ans << endl;
+}
