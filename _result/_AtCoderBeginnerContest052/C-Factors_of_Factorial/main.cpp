7aab15b26a19af72e9909bca2cd6546174c316de
Yoyuuhi <xijie931025@gmail.com>
2023-11-17 01:14:03 +0900

    AtCoderBeginnerContest052 C-Factors of Factorial

---
+++ _result/_AtCoderBeginnerContest052/C-Factors_of_Factorial/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+typename enable_if<is_same_v<T, ll> || is_same_v<T, int> ||
+                       is_same_v<T, unsigned int>,
+                   void>::type
+out(T value) {
+  cout << value << endl;
+}
+template <typename T>
+typename enable_if<is_same_v<T, mint>, void>::type out(T value) {
+  cout << value.val() << endl;
+}
+template <typename T>
+typename enable_if<is_same_v<T, vector<ll>> || is_same_v<T, vector<int>>,
+                   void>::type
+out(T value) {
+  for (auto v : value) cout << v << " ";
+  cout << endl;
+}
+
+template <typename T>
+vector<pair<T, T>> prime_factor(T n) {
+  vector<pair<T, T>> ret;
+  for (T i = 2; i * i <= n; i++) {
+    if (n % i != 0) continue;
+    T tmp = 0;
+    while (n % i == 0) {
+      tmp++;
+      n /= i;
+    }
+    ret.push_back(make_pair(i, tmp));
+  }
+  if (n != 1) ret.push_back(make_pair(n, 1));
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n;
+  cin >> n;
+  vector<ll> mem(n + 1, 1);
+  rep2(i, 1, n + 1) {
+    auto ps = prime_factor(i);
+    for (auto p : ps) mem[p.first] += p.second;
+  }
+  mint ans = 1;
+  for (auto m : mem) ans *= m;
+  out(ans);
+}
