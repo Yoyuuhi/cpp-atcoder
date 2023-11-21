c2f68138a7c22af9aa5c6d27720fbe87e33c0756
Yoyuuhi <xijie931025@gmail.com>
2023-11-22 00:13:45 +0900

    AtCoderBeginnerContest052 D-Walk and Teleport

---
+++ _result/_AtCoderBeginnerContest052/D-Walk_and_Teleport/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+ll ni() {
+  ll ret;
+  cin >> ret;
+  return ret;
+}
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n, a, b;
+  cin >> n >> a >> b;
+  auto xs = nis(n);
+
+  ll ans = 0;
+  rep(i, n - 1) ans += min(a * (xs[i + 1] - xs[i]), b);
+  out(ans);
+}
