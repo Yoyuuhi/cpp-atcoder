6dcc2ef5fece837a86140c9408d53890a2314daa
Yoyuuhi <xijie931025@gmail.com>
2023-11-25 21:01:20 +0900

    AtCoderBeginnerContest330 A-Counting Passes

---
+++ _result/_AtCoderBeginnerContest330/A-Counting_Passes/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, l;
+  cin >> n >> l;
+  auto as = nis(n);
+  int ans = 0;
+  for (auto a : as)
+    if (a >= l) ans++;
+  out(ans);
+}
