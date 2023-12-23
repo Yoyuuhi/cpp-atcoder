643625c28be762bab6d2405feca828437ac2cc99
Yoyuuhi <xijie931025@gmail.com>
2023-12-23 21:21:35 +0900

    AtCoderBeginnerContest334 B-Christmas Trees

---
+++ _result/_AtCoderBeginnerContest334/B-Christmas_Trees/main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T pow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll a, m, l, r;
+  cin >> a >> m >> l >> r;
+  ll ans = 0;
+
+  ll k1 = (l - a) / m;
+  if (k1 * m < l - a) k1++;
+  ll k2 = (r - a) / m;
+  if (k2 * m > r - a) k2--;
+
+  out(k2 - k1 + 1);
+}
