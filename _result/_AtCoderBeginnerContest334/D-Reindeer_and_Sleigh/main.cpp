9b906f820e8c287a51dd311a20bb19537fbebf92
Yoyuuhi <xijie931025@gmail.com>
2023-12-23 21:45:54 +0900

    AtCoderBeginnerContest334 D-Reindeer and Sleigh

---
+++ _result/_AtCoderBeginnerContest334/D-Reindeer_and_Sleigh/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T pow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n, q;
+  cin >> n >> q;
+  auto rs = nis(n);
+  sort(all(rs));
+  vector<ll> sumr(n, 0);
+  sumr[0] = rs[0];
+  rep2(i, 1, n) {
+    sumr[i] += rs[i];
+    sumr[i] += sumr[i - 1];
+  }
+  rep(i, q) {
+    ll x;
+    cin >> x;
+    int idx = lower_bound(all(sumr), x + 1) - sumr.begin();
+    out(idx);
+  }
+}
