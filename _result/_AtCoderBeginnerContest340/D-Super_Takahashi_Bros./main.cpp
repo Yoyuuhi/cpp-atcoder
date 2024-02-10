9bedf051268209e1ae37e8dc9133b73e5b873e34
Yoyuuhi <xijie931025@gmail.com>
2024-02-10 21:21:29 +0900

    AtCoderBeginnerContest340 D-Super Takahashi Bros.

---
+++ _result/_AtCoderBeginnerContest340/D-Super_Takahashi_Bros./main.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS) \
+  for (auto X : XS) cout << X << " ";
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+struct Edge {
+  long long to;
+  long long cost;
+};
+using Graph = vector<vector<Edge>>;
+using P = pair<ll, int>;
+void dijkstra(const Graph &G, int s, vector<long long> &dis) {
+  int N = G.size();
+  dis.resize(N, INFLL);
+  priority_queue<P, vector<P>, greater<P>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty()) {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first) {
+      continue;
+    }
+    for (auto &e : G[v]) {
+      if (dis[e.to] > dis[v] + e.cost) {
+        dis[e.to] = dis[v] + e.cost;
+        pq.emplace(dis[e.to], e.to);
+      }
+    }
+  }
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n;
+  cin >> n;
+
+  Graph g(n);
+  rep(i, n - 1) {
+    ll a, b, x;
+    cin >> a >> b >> x;
+    x--;
+    g[i].push_back({i + 1, a});
+    g[i].push_back({x, b});
+  }
+  vector<ll> dis;
+  dijkstra(g, 0, dis);
+  out(dis[n - 1]);
+}
