29d287361bcbc2ce3bbe42490d760cc386aca77a
Yoyuuhi <xijie931025@gmail.com>
2024-02-11 00:30:43 +0900

    AtCoderBeginnerContest194 A-I Scream

---
+++ _result/_AtCoderBeginnerContest194/A-I_Scream/main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS) \
+  for (auto X : XS) cout << X << " ";
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int a, b;
+
+  cin >> a;
+  cin >> b;
+
+  if (a + b >= 15 && b >= 8) {
+    out(1);
+  } else if (a + b >= 10 && b >= 3) {
+    out(2);
+  } else if (a + b >= 3) {
+    out(3);
+  } else {
+    out(4);
+  }
+}
