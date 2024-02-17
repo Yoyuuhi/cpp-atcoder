05cd4dcde74865a1c39b4ddf5f52808d95f7542c
Yoyuuhi <xijie931025@gmail.com>
2024-02-17 21:37:51 +0900

    AtCoderBeginnerContest341 E-Alternating String

---
+++ _result/_AtCoderBeginnerContest341/E-Alternating_String/main.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS) \
+  for (auto X : XS) cout << X << " ";
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int op(int a, int b) { return max(a, b); }
+int e() { return 0; }
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, q;
+  cin >> n >> q;
+  string s;
+  cin >> s;
+
+  vector<int> as(n, 0);
+  rep(i, n - 1) {
+    if (s[i] == s[i + 1]) as[i]++;
+  }
+
+  segtree<int, op, e> st(as);
+  rep(i, q) {
+    int que, l, r;
+    cin >> que >> l >> r;
+    l--;
+    r--;
+    if (que == 1) {
+      if (l > 0) {
+        st.set(l - 1, (st.get(l - 1) + 1) % 2);
+      }
+      if (r + 1 < n) {
+        st.set(r, (st.get(r) + 1) % 2);
+      }
+    }
+    if (que == 2) {
+      int num = st.prod(l, r);
+      if (num == 0) {
+        out("Yes");
+      } else {
+        out("No");
+      }
+    }
+  }
+}
