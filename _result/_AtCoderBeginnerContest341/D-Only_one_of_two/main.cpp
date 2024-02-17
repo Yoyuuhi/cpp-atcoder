c74d840e5c04b452ecc3f79ea207241021ab70fa
Yoyuuhi <xijie931025@gmail.com>
2024-02-17 21:28:18 +0900

    AtCoderBeginnerContest341 D-Only one of two

---
+++ _result/_AtCoderBeginnerContest341/D-Only_one_of_two/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS) \
+  for (auto X : XS) cout << X << " ";
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+template <typename T>
+T bs(T ok, T ng, bool f(T)) {
+  if (!f(ok)) return -1;
+  if (f(ng)) return ng;
+  while (abs(ok - ng) > 1) {
+    auto mid = (ok + ng) / 2;
+    if (f(mid)) {
+      ok = mid;
+    } else {
+      ng = mid;
+    }
+  }
+  return ok;
+}
+
+long long gcd(long long a, long long b) {
+  if (b == 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+ll n, m, k;
+bool calc(ll num) {
+  return num / n + num / m - (num / (n * m / gcd(n, m))) * 2 >= k;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  cin >> n >> m >> k;
+  ll ans = bs(INFLL, ll(1), calc);
+  out(ans);
+}
