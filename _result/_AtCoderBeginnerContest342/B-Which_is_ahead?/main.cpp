74f34e6abf4939f7521a65c42046189743ee5771
Yoyuuhi <xijie931025@gmail.com>
2024-02-24 21:06:15 +0900

    AtCoderBeginnerContest342 B-Which is ahead?

---
+++ _result/_AtCoderBeginnerContest342/B-Which_is_ahead?/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS) \
+  for (auto X : XS) cout << X << " ";
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  auto ps = nis(n);
+  int q;
+  cin >> q;
+  rep(i, q) {
+    int a, b;
+    cin >> a >> b;
+    int pa, pb;
+    rep(i, n) {
+      int p = ps[i];
+      if (p == a) pa = i;
+      if (p == b) pb = i;
+    }
+    if (pa > pb) swap(a, b);
+    out(a);
+  }
+}
