eac84c494d0b4b4f4dc60589898f701cd3d98c4f
Yoyuuhi <xijie931025@gmail.com>
2024-02-24 21:15:14 +0900

    AtCoderBeginnerContest342 C-Many Replacement

---
+++ _result/_AtCoderBeginnerContest342/C-Many_Replacement/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS) \
+  for (auto X : XS) cout << X << " ";
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int q;
+  cin >> q;
+
+  vector<int> mem(26);
+  rep(i, 26) mem[i] = i;
+  rep(i, q) {
+    char c, d;
+    cin >> c >> d;
+    rep(j, 26) {
+      if (c - 'a' == mem[j]) mem[j] = d - 'a';
+    }
+  }
+  for (auto c : s) cout << char('a' + mem[c - 'a']);
+  outl;
+}
