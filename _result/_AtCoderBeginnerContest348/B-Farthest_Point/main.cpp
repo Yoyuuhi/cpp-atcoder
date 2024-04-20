2b1afd6a7bad24bced02850da3607535929cf01f
Yoyuuhi <xijie931025@gmail.com>
2024-04-20 13:18:11 +0900

    AtCoderBeginnerContest348 B-Farthest Point

---
+++ _result/_AtCoderBeginnerContest348/B-Farthest_Point/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  vector<vector<int>> xys(n);
+  rep(i, n) {
+    int x, y;
+    cin >> x >> y;
+    xys[i] = {x, y};
+  }
+
+  rep(i, n) {
+    int dis = 0;
+    int ans = -1;
+    rep(j, n) {
+      int d = tpow(xys[i][0] - xys[j][0], 2) + tpow(xys[i][1] - xys[j][1], 2);
+      if (d > dis) {
+        dis = d;
+        ans = j + 1;
+      }
+    }
+    out(ans);
+  }
+}
