1cfff95d38a4a233e51c72191e1d0f2581a768a9
Yoyuuhi <xijie931025@gmail.com>
2024-05-11 21:41:30 +0900

    AtCoderBeginnerContest353 D-Another Sigma Problem

---
+++ _result/_AtCoderBeginnerContest353/D-Another_Sigma_Problem/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n;
+  cin >> n;
+  auto as = nis(n);
+
+  mint ans = 0;
+  mint sum = as[n - 1];
+  vector<int> mem(11, 0);
+  mem[to_string(as[n - 1]).size()]++;
+  for (int i = n - 2; i >= 0; i--) {
+    ll a = as[i];
+    rep2(i, 1, 11) { ans += mint(a) * tpow(mint(10), i) * mint(mem[i]); }
+
+    mem[to_string(a).size()]++;
+    ans += sum;
+    sum += a;
+  }
+  out(ans.val());
+}
