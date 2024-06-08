b57841d2eece3ddda14fa51e3363a9b5483df2c5
Yoyuuhi <xijie931025@gmail.com>
2024-06-08 22:53:16 +0900

    AtCoderBeginnerContest357 D-88888888

---
+++ _result/_AtCoderBeginnerContest357/D-88888888/main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint998244353;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, ll n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll N;
+  cin >> N;
+  string s = to_string(N);
+  ll d = s.size();
+
+  mint pow10_d = tpow(mint(10), d);
+  mint pow10_dN = tpow(tpow(mint(10), N), d);
+  mint ans = mint(N) * (pow10_dN - 1) / (pow10_d - 1);
+  out(ans.val());
+}
