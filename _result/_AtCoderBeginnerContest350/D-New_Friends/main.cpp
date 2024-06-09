a153ede691dc50e7007cae096e249bc88639b06c
Yoyuuhi <xijie931025@gmail.com>
2024-06-09 18:53:12 +0900

    AtCoderBeginnerContest350 D-New Friends

---
+++ _result/_AtCoderBeginnerContest350/D-New_Friends/main.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n, m;
+  cin >> n >> m;
+  vector<vector<ll>> ABS;
+  dsu d(n);
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    ABS.push_back({a, b});
+    d.merge(a, b);
+  }
+
+  vector<ll> mem(n, 0);
+  map<int, bool> ls;
+  for (auto ab : ABS) {
+    int a = ab[0];
+    mem[d.leader(a)]++;
+    ls[d.leader(a)] = true;
+  }
+
+  ll ans = 0;
+  for (auto L : ls) {
+    auto l = L.first;
+    ans += ll(d.size(l)) * (d.size(l) - 1) / 2 - mem[l];
+  }
+  out(ans);
+}
