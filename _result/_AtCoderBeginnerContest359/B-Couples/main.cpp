bf0d827c3065dbcfc735edd0f73d0fe890fbdb14
Yoyuuhi <xijie931025@gmail.com>
2024-06-22 21:06:18 +0900

    AtCoderBeginnerContest359 B-Couples

---
+++ _result/_AtCoderBeginnerContest359/B-Couples/main.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  auto as = nis(2 * n);
+
+  int ans = 0;
+  rep2(c, 1, n + 1) {
+    int ia = -1, ib = -1;
+    rep(i, 2 * n) {
+      int a = as[i];
+      if (c != a) continue;
+      if (ia == -1) {
+        ia = i;
+      } else {
+        ib = i;
+        if (ia + 2 == ib) ans++;
+      }
+    }
+  }
+  out(ans);
+}
