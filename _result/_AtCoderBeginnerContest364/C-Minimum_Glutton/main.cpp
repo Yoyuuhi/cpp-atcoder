11ad43b673f69e274402a2068b4c18677ba95ca2
Yoyuuhi <xijie931025@gmail.com>
2024-07-27 21:15:24 +0900

    AtCoderBeginnerContest364 C-Minimum Glutton

---
+++ _result/_AtCoderBeginnerContest364/C-Minimum_Glutton/main.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n, x, y;
+  cin >> n >> x >> y;
+  auto as = nis(n);
+  auto bs = nis(n);
+
+  sort(all(as), greater<ll>());
+  sort(all(bs), greater<ll>());
+
+  ll ans = n;
+  ll cnt = 0;
+  rep(i, n) {
+    ll a = as[i];
+    cnt += a;
+    if (cnt > x) {
+      ans = min(ans, ll(i + 1));
+      break;
+    }
+  }
+
+  cnt = 0;
+  rep(i, n) {
+    ll b = bs[i];
+    cnt += b;
+    if (cnt > y) {
+      ans = min(ans, ll(i + 1));
+      break;
+    }
+  }
+  out(ans);
+}
