3f8e9cdf73f859297d153e6392d6637c28b81e8f
Yoyuuhi <xijie931025@gmail.com>
2024-08-10 21:18:17 +0900

    AtCoderBeginnerContest366 C-Balls and Bag Query

---
+++ _result/_AtCoderBeginnerContest366/C-Balls_and_Bag_Query/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int q;
+  cin >> q;
+  map<int, int> mem;
+  rep(i, q) {
+    int que;
+    cin >> que;
+    if (que == 1) {
+      int x;
+      cin >> x;
+      mem[x]++;
+    }
+    if (que == 2) {
+      int x;
+      cin >> x;
+      mem[x]--;
+      if (mem[x] == 0) mem.erase(x);
+    }
+    if (que == 3) {
+      out(mem.size());
+    }
+  }
+}
