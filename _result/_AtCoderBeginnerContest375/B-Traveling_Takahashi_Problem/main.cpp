4c33f7107958394a7a93d0ade1e66c27a4ed1fca
Yoyuuhi <xijie931025@gmail.com>
2024-10-12 23:03:31 +0900

    AtCoderBeginnerContest375 B-Traveling Takahashi Problem

---
+++ _result/_AtCoderBeginnerContest375/B-Traveling_Takahashi_Problem/main.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+
+  double ans = 0;
+  double xx = 0, yy = 0;
+  rep(i, n) {
+    double x, y;
+    cin >> x >> y;
+    ans += sqrt(tpow(xx - x, 2) + tpow(yy - y, 2));
+    xx = x;
+    yy = y;
+  }
+  ans += sqrt(tpow(xx, 2) + tpow(yy, 2));
+  cout << fixed << setprecision(16) << ans << endl;
+}
