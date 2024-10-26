c0aeb05d6701f33028c848ed4b304b9f3eeab518
Yoyuuhi <xijie931025@gmail.com>
2024-10-26 22:03:48 +0900

    AtCoderBeginnerContest377 C-Avoid Knight Attack

---
+++ _result/_AtCoderBeginnerContest377/C-Avoid_Knight_Attack/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll n, m;
+  cin >> n >> m;
+
+  set<pair<ll, ll>> st;
+  vector<vector<ll>> vs = {
+      {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1},
+  };
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    st.emplace(a, b);
+
+    for (auto v : vs) {
+      auto aa = a + v[0], bb = b + v[1];
+      if (aa >= 0 && aa < n && bb >= 0 && bb < n) st.emplace(aa, bb);
+    }
+  }
+  out(n * n - st.size());
+}
