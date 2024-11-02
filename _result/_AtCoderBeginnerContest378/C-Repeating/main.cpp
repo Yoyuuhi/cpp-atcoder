1ad810171f95c6710b5ba9719ef61c0516a36cb9
Yoyuuhi <xijie931025@gmail.com>
2024-11-02 21:13:33 +0900

    AtCoderBeginnerContest378 C-Repeating

---
+++ _result/_AtCoderBeginnerContest378/C-Repeating/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  auto as = nis(n);
+
+  map<int, int> mem;
+  vector<int> bs(n, -1);
+  rep(i, n) {
+    int a = as[i];
+    if (mem.find(a) == mem.end()) {
+      mem[a] = i + 1;
+      continue;
+    }
+
+    bs[i] = mem[a];
+    mem[a] = i + 1;
+  }
+  outs(bs);
+}
