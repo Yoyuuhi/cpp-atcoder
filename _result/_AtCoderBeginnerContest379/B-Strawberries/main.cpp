4e736076b49da6a2531e88ba201d4bacf73505b3
Yoyuuhi <xijie931025@gmail.com>
2024-11-15 22:04:49 +0900

    AtCoderBeginnerContest379 B-Strawberries

---
+++ _result/_AtCoderBeginnerContest379/B-Strawberries/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n, k;
+  cin >> n >> k;
+  string s;
+  cin >> s;
+
+  int l = -1;
+  int ans = 0;
+  rep(i, n) {
+    auto c = s[i];
+    if (c == 'X') l = i;
+    if (i - l == k) {
+      ans++;
+      l = i;
+    }
+  }
+  out(ans);
+}
