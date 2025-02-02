92f76abc7b079772b167d00d129850fe600d1d72
Yoyuuhi <xijie931025@gmail.com>
2025-02-02 20:13:59 +0900

    AtCoderBeginnerContest388 C-Various Kagamimochi

---
+++ _result/_AtCoderBeginnerContest388/C-Various_Kagamimochi/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  int n;
+  cin >> n;
+  auto as = nis(n);
+
+  int r = 0;
+  ll ans = 0;
+  rep(i, n) {
+    while (r < n && as[r] < 2 * as[i]) r++;
+    if (r == n) break;
+    ans += n - r;
+  }
+  out(ans);
+}
