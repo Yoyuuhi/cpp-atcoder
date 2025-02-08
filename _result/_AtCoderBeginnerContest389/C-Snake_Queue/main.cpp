485667b7bea361b889f6076a371e5cb8b4937395
Yoyuuhi <xijie931025@gmail.com>
2025-02-08 16:33:00 +0900

    AtCoderBeginnerContest389 C-Snake Queue

---
+++ _result/_AtCoderBeginnerContest389/C-Snake_Queue/main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+#define out(X) cout << (X) << endl;
+#define outs(XS)                      \
+  for (auto X : XS) cout << X << " "; \
+  cout << endl;
+#define outl cout << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+const int INF = numeric_limits<int>::max() / 2;
+const ll INFLL = numeric_limits<ll>::max() / 2;
+template <typename T>
+vector<T> nis(T n) {
+  vector<T> ret(n);
+  rep(i, n) cin >> ret[i];
+  return ret;
+}
+template <typename T>
+T tpow(T x, int n) {
+  T ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef __LOCAL
+  freopen("input", "r", stdin);
+#endif
+
+  ll q;
+  cin >> q;
+
+  vector<ll> mem;
+  ll sum1 = 0, sum2 = 0, idx = 0;
+  rep(i, q) {
+    ll que;
+    cin >> que;
+    if (que == 1) {
+      ll l;
+      cin >> l;
+      mem.push_back(sum1);
+      sum1 += l;
+    }
+    if (que == 2) {
+      idx++;
+      sum2 = mem[idx];
+    }
+    if (que == 3) {
+      int k;
+      cin >> k;
+      k--;
+      out(mem[idx + k] - sum2);
+    }
+  }
+}
